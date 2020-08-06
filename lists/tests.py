7f213b2e8c3e5f22a50441d04c9dcb1ef46b7fa8
vishal <vishal.02jan@gmail.com>
2020-08-06 14:40:15 +0530

    Basic view now returns minimal html.

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -10,8 +11,24 @@ class SmokeTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
 
 # pytest type tests
 def test_root_url_resolves_to_home_page_view():
     found = resolve('/')
     assert found.func == home_page
+
+
+def test_home_page_returns_correct_html():
+    request = HttpRequest()
+    response = home_page(request)
+    html = response.content.decode('utf8')
+    assert html.startswith('<html>') and html.endswith('</html>')
+    assert '<title>To-Do lists</title>' in html
