74dd7b7f4f4746400fb4e44dfa679c9df8d4c100
vishal <vishal.02jan@gmail.com>
2020-08-13 14:45:35 +0530

    view function now handles post request

--- lists/tests.py
+++ lists/tests.py
@@ -8,8 +8,19 @@ class SmokeTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'lists/home.html')
+
 
 # pytest type tests
 def test_home_page_returns_correct_html(client):
     response = client.get('/')
+    assertTemplateUsed(response, 'lists/home.html')
+
+
+def test_can_save_a_POST_request(client):
+    response = client.post('/', {'item_text': 'A new list item'})
+    assert 'A new list item' in response.content.decode()
     assertTemplateUsed(response, 'lists/home.html')
--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'lists/home.html')
+    return render(
+            request,
+            'lists/home.html',
+            {'new_item_text': request.POST.get('item_text', '')}
+        )
